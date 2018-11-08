647bfd55117e68fe441308e4a23cff41d09bb7d2
Andria Njoku <andrianjoku@gmail.com>
2018-11-08 18:04:32 +0000

    Added variables relating to calculating costs and profit. created a function wich returns employees names and their salary. I wanmt to see if there is a way of flattening this output and create a map of name and salary based on some timeframe.

--- Database_Query.js
+++ Database_Query.js
@@ -15,25 +15,51 @@ MongoClient.connect(url, function(err, db) {
   if (err) throw err;
   console.log("Database created!");
   
+ 
   
   var dbo = db.db("mydb");
-
-// here i will hold all of my database queries 
+  
+//here i will hold all of my database queries 
 
 
 //global variables 
 // this will be for a two month period since we are assumed to have only be open
   //this long and have flights covering this period
   
-var employeepay=0;
- 
+  
+  
+  // Holds the the employees pay. The business is assumed to have started beginning of October and 
+  //we are going through a trial period through November and december. Employees are currently on
+  //wage which will increase when the business starts flying i will need to account for this.
+  
+  
+  // Global variables 
+  
+  var employeepaytraining=0;
+
+  var employeepaytrading=0;
+
+  var rentoffacilities=0;
+
+  var upkeepestimate=0;
+
+  var vehiclemaintainenceestimate=0;
+
+  var fuelaccurateestimate=0;
+  
+  
+  var airportcosts=rentoffacilities+upkeepestimate+vehiclemaintainenceestimate+fuelaccurateestimate;
+  
+  
+  
+  //some helper functions
 
 //this function will simply print to the console 
- function printtoconsole(a){
+ function print(a){
 	 
 	 console.log(a);
 	 
- }
+ };
  
 //function for printing output of queries to an external file 
  
@@ -41,12 +67,37 @@ var employeepay=0;
  function printtotextfile( a){
 	 
 	  
- }
+ };
  
  
- dbo.
+ 
+ //Main functions to interact with 
+ 
+
+ 
+ function calculateWagePerMonth(){
+	 
+	  dbo.collection("employees").find({}, { projection: { _id: 0, name: 1, salary: 1 } }).toArray(function(err, result) {
+		    if (err) throw err;
+		    
+		    
+		   print(result)
+     
+	  });
+ };
+ 
+ 
+ 
+ 
+
+//test1
+  
+calculateWagePerMonth();
   
   
-  //Pilots are assigned to planes if they are available and they also take a few hours break and will also fly the plane back.
-  //will assign custom objectID to pilot fields in order to join them relationally with flights and flights with booking.
   
+
+  
+  db.close();
+  
+});
