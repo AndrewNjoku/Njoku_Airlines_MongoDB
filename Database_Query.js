6d3145126f012c25178940bcb7c6461fe4641912
Andria Njoku <andrianjoku@gmail.com>
2018-11-08 17:10:47 +0000

    I have beefed up my query script, adding two functions. One is to simply print to console and one is going to print to an external file. Nothing too complicated but i want to get the basics right and learn the structure of Javascript outside of Web-based using node.js. i have also updated the query script with the correct headers to connect to the database.

--- Database_Query.js
+++ Database_Query.js
@@ -1,48 +1,52 @@
 
 
 
- function idgenerator(max) {
-	  return Math.floor(Math.random() * Math.floor(max));
-	}
+//connect to mongodb database
+
+var MongoClient = require('mongodb').MongoClient;
+
+var ObjectId = require('mongodb');
+
+var url = "mongodb://localhost:27017/mydb";
+
+
+
+MongoClient.connect(url, function(err, db) {
+  if (err) throw err;
+  console.log("Database created!");
   
+  
+  var dbo = db.db("mydb");
+
+// here i will hold all of my database queries 
+
+
+//global variables 
+// this will be for a two month period since we are assumed to have only be open
+  //this long and have flights covering this period
+  
+var employeepay=0;
  
+
+//this function will simply print to the console 
  function printtoconsole(a){
 	 
 	 console.log(a);
+	 
  }
+ 
+//function for printing output of queries to an external file 
+ 
+ 
+ function printtotextfile( a){
+	 
+	  
+ }
+ 
+ 
+ dbo.
   
   
   //Pilots are assigned to planes if they are available and they also take a few hours break and will also fly the plane back.
   //will assign custom objectID to pilot fields in order to join them relationally with flights and flights with booking.
   
-  var johnpilotid = idgenerator(999);
-  var peterpilotid = idgenerator(999);
-  var amypilotid = idgenerator(999);
-  var hannahpilotid= idgenerator(999);
-  
-  
-  
-  
-  for(i=0;i<10;i++){
-	  
-  
-  printtoconsole(johnpilotid);
-  
-  
-  }
-  
-  
-
-  for(i=0;i<10;i++){
-	  
-  
-  printtoconsole(johnpilotid);
-  
-  
-  }
-  
-  
-
-
-
- 
